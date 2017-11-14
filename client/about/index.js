9bce7dc416361a99add617494287312071857d5c
boybutcher <BYRONdeLARGE@yahoo.com>
2017-11-14 13:23:51 -0800

    helper function to create paragraphs

--- client/about/index.js
+++ client/about/index.js
@@ -1,3 +1,9 @@
+const aboutText = [
+  `Hi! I'm Bryan.`,
+  `One of my earliest memories is watching my dad, an aerospace engineer, tinkering in the garage with remote controlled cars & boats and my mom gardening. I think both of these experiences are indicitive of who I am as a person.`,
+  `For the last year and a half, I've been primarily a JavaScript developer but I take pride in my flexibility and willingness to learn new languages.`,
+];
+
 const aboutHeader = document.createElement('div');
 aboutHeader.className = 'about-header';
 aboutHeader.innerHTML = 'about.';
@@ -6,10 +12,22 @@ const devImg = document.createElement('img');
 devImg.className = 'dev-img'
 devImg.src = 'assets/bqn.JPG';
 
+const createParagraphs = (paragraph) => {
+  const paraItem = document.createElement('p');
+  paraItem.innerHTML = paragraph;
+  return paraItem;  
+};
+
+const stringToPara = (parent, children) => {
+  for (let i = 0; i < children.length; i++) {
+    parent.appendChild(createParagraphs(children[i]));
+  }
+}
+
 const aboutDesc = document.createElement('div');
 aboutDesc.className = 'about-description';
-aboutDesc.innerHTML = 'Bryan is the best developer I ever did see.'
-
+stringToPara(aboutDesc, aboutText);
+  
 const aboutContainer = document.createElement('div');
 aboutContainer.className = 'about-container';
 aboutContainer.appendChild(devImg);
